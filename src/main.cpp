df21d6c60a60f84776b88534157f0b0717ea8267
Rico Koschmitzky <contact@ricokoschmitzky.com>
2020-04-06 20:00:46 +0200

    wip to write some first image

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,30 @@ int main() {
     AtNode *options = AiUniverseGetOptions();
     AiNodeSetBool(options, "skip_license_check", true);
 
+    AiRender(AI_RENDER_MODE_FREE);
+
+    /*
+    const char *driver_type_name = AiNodeEntryGetName(AiFindDriverType("jpg"));
+    AtNode *driver = AiNode(driver_type_name, "simple_driver");
+    AiDriverInitialize(driver, false);
+
+    AiNodeSetStr(driver, "filename", "lambda.jpg");
+    AtArray *outputs= AiArray(4, 1, AI_TYPE_STRING, "RGB", "RGB", "simple_filter", "simple_driver");
+
+    const char *filter_name = "gaussian_filter";
+    AtNode *filter = AiNode(filter_name, "simple_filter");
+    AiNodeSetArray(options, "outputs", outputs);
+
+    char *output_name = "RGB";
+    int output_type = AI_TYPE_RGB;
+
+    void *scanlines[480] = {};
+
+    for (int i = 0; i < 480; ++i) {
+        void *pixeldata = AiMalloc(640 * sizeof(float) * 3);
+    }
+    AiOutputIteratorGetNext(NULL, &output_name, &output_type, pixeldata);
+    */
 
     AtVector origin = AtVector(0, 0, 8);
     AtVector direction = AtVector(0, -0.01, -1);
