10f417fa1af1ba27291431f4e3ca51eb4b4023e8
dennisalbus <code@dennisalbus.com>
2020-04-10 18:45:06 +0200

    Change build.sh to accomodate for include paths

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 clang main.cpp \
     -std=c++14 \
     -lm -lstdc++ \
-    -I../thirdparty/Arnold/include \
-    -L../thirdparty/Arnold/bin -lai \
+    -I../thirdparty/arnold_linux/include \
+    -I../thirdparty/stb \
+    -L../thirdparty/arnold_linux/bin -lai \
     -o ../build/lambda
