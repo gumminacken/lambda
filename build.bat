894900c7136bc3489240fb713aa8b1b2e8f02685
Rico Koschmitzky <contact@ricokoschmitzky.com>
2020-04-13 12:43:59 +0200

    update cpp properties
    add stb include path to build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 pushd build
 call "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-set ARNOLD_PATH=../thirdparty/arnold_windows
-cl ..\src\main.cpp /I %ARNOLD_PATH%\include %ARNOLD_PATH%\lib\ai.lib /link /out:lambda.exe
+set THIRDPARTY=../thirdparty
+cl ..\src\main.cpp /I %THIRDPARTY%\arnold_windows\include /I %THIRDPARTY%\stb %THIRDPARTY%\arnold_windows\lib\ai.lib /link /out:lambda.exe
 popd
 
