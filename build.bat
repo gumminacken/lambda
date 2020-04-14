6a4198a92f0bf5aca982d574ae6f23526f896c76
Rico Koschmitzky <contact@ricokoschmitzky.com>
2020-04-14 18:19:04 +0200

    add stb include paths

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 pushd build
 call "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 set THIRDPARTY=../thirdparty
-cl ..\src\main.cpp /I %THIRDPARTY%\arnold_windows\include /I %THIRDPARTY%\stb %THIRDPARTY%\arnold_windows\lib\ai.lib /link /out:lambda.exe
+cl ..\src\main.cpp /Zi /I %THIRDPARTY%\arnold_windows\include /I %THIRDPARTY%\stb %THIRDPARTY%\arnold_windows\lib\ai.lib /link /out:lambda.exe
 popd
 
