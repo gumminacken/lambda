7851deab9aac48f4d1d81ac81e4d3b25b560a712
dennisalbus <code@dennisalbus.com>
2020-04-14 19:24:45 +0200

    Add buckets to the renderer for multithreading

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 
+#include <math.h>
 #include <ai.h>
 #include <stb_image_write.h>
+
+struct Bucket {
+    int index;
+    int min[2];
+    int max[2];
+    uint8_t *pixels;
+    uint8_t *current_pixel;
+};
 
 int main() {
     AiBegin();
@@ -14,86 +23,71 @@ int main() {
 
     AiRender(AI_RENDER_MODE_FREE);
 
-    /*
-    const char *driver_type_name = AiNodeEntryGetName(AiFindDriverType("jpg"));
-    AtNode *driver = AiNode(driver_type_name, "simple_driver");
-    AiDriverInitialize(driver, false);
-
-    AiNodeSetStr(driver, "filename", "lambda.jpg");
-    AtArray *outputs= AiArray(4, 1, AI_TYPE_STRING, "RGB", "RGB", "simple_filter", "simple_driver");
-
-    const char *filter_name = "gaussian_filter";
-    AtNode *filter = AiNode(filter_name, "simple_filter");
-    AiNodeSetArray(options, "outputs", outputs);
-
-    char *output_name = "RGB";
-    int output_type = AI_TYPE_RGB;
-
-    void *scanlines[480] = {};
-
-    for (int i = 0; i < 480; ++i) {
-        void *pixeldata = AiMalloc(640 * sizeof(float) * 3);
-    }
-    AiOutputIteratorGetNext(NULL, &output_name, &output_type, pixeldata);
-    */
-
-    // TODO: Use an actual camera to create proper rays
-    // TODO: Use a sampler to generate better subpixel samples
-    /*
-    AtNode *cam = AiUniverseGetCamera();
-    AtCameraInput in = {};
-    AtCameraOutput out = {};
-
-    AtCameraNodeMethods *methods = (AtCameraNodeMethods *)cam;
-    (methods->CreateRay)(cam, in, out, 0);
-    */
-
-    int w = 640;
-    int h = 480;
+    int w = 320;
+    int h = 320;
     int samples = 4;
 
+    int bucket_size = 160;
 
     AtSampler *sampler = AiSampler(12345, samples, 2);
     AtShaderGlobals *sample_globals = AiShaderGlobals();
 
     AtVector origin = AtVector(0, 5, 10);
 
+
     // NOTE: Why is this not segfaulting if we deliberately choose a buffer that's too small?
     uint8_t *buffer = (uint8_t *)malloc(w * h * 3 * sizeof(char));
     uint8_t *curr = buffer;
 
+    int x_buckets = ceil(w / (float)bucket_size);
+    int y_buckets = ceil(h / (float)bucket_size);
 
-    for (int y = 0; y < h; ++y) {
-        for (int x = 0; x < w; ++x) {
-            AtSamplerIterator *sampler_iter = AiSamplerIterator(sampler, sample_globals);
-            float sample[2] = {};
-            AtVector color = AtVector(0, 0, 0);
+    for (int bucket_index = 0; bucket_index < x_buckets * y_buckets; ++bucket_index) {
+        Bucket bucket {
+            bucket_index,
+            (bucket_index % x_buckets)  * bucket_size, (bucket_index / x_buckets)  * bucket_size,
+            0, 0,
+            (uint8_t *)malloc(pow(bucket_size, 2) * 3 * sizeof(char)),
+            0
+        };
+        bucket.max[0] = bucket.min[0] + bucket_size;
+        bucket.max[1] = bucket.min[1] + bucket_size;
+        bucket.current_pixel = bucket.pixels;
 
-            while (AiSamplerGetSample(sampler_iter, sample)) {
-                AtVector direction = AtVector(
-                    1 - (((float)x / w) * 2) + (sample[0] / w),
-                    1 - (((float)y / h) * 2) + (sample[1] / h),
-                    -1
-                );
+        for (int y = bucket.min[1]; y < bucket.max[1]; ++y) {
+            for (int x = bucket.min[0]; x < bucket.max[0]; ++x) {
+                AtSamplerIterator *sampler_iter = AiSamplerIterator(sampler, sample_globals);
+                float sample[2] = {};
+                AtVector color = AtVector(0, 0, 0);
 
-                AiV3Normalize(direction);
-                AtShaderGlobals *initial_globals = AiShaderGlobals();
-                AtRay ray = AiMakeRay(AI_RAY_CAMERA, origin, &direction, AI_BIG, initial_globals);
-                AtShaderGlobals *trace_globals = AiShaderGlobals();
+                while (AiSamplerGetSample(sampler_iter, sample)) {
+                    AtVector direction = AtVector(
+                        1 - (((float)x / w) * 2) + (sample[0] / w),
+                        1 - (((float)y / h) * 2) + (sample[1] / h),
+                        -1
+                    );
 
-                bool hit = false;
-                hit = AiTraceProbe(ray, trace_globals);
+                    AiV3Normalize(direction);
+                    AtShaderGlobals *initial_globals = AiShaderGlobals();
+                    AtRay ray = AiMakeRay(AI_RAY_CAMERA, origin, &direction, AI_BIG, initial_globals);
+                    AtShaderGlobals *trace_globals = AiShaderGlobals();
 
-                color += trace_globals->N / samples;
+                    bool hit = false;
+                    hit = AiTraceProbe(ray, trace_globals);
+
+                    color += trace_globals->N / samples;
+                }
+
+                *bucket.current_pixel++ = (char)(color.x * 255);
+                *bucket.current_pixel++ = (char)(color.y * 255);
+                *bucket.current_pixel++ = (char)(color.z * 255);
             }
-
-            *curr++ = (char)(color.x * 255);
-            *curr++ = (char)(color.y * 255);
-            *curr++ = (char)(color.z * 255);
         }
+        AiMsgInfo("YAY");
+        memcpy(buffer + bucket_index * (int)pow(bucket_size, 2), bucket.pixels, sizeof(char) * 3 * pow(bucket_size, 2));
     }
 
-    stbi_write_jpg("test.jpg", w, h, 3, buffer, 100);
+    stbi_write_jpg("test2.jpg", w, h, 3, buffer, 100);
 
     AiRenderEnd();
     AiEnd();
