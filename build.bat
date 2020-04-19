5083ad44ba249362fb4d4637fd28e97f2559c525
Rico Koschmitzky <contact@ricokoschmitzky.com>
2020-04-19 12:51:14 +0200

    remove basically everything!!!

--- build.bat
+++
@@ -1,6 +0,0 @@
-pushd build
-call "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-set THIRDPARTY=../thirdparty
-cl ..\src\main.cpp /Zi /I %THIRDPARTY%\arnold_windows\include /I %THIRDPARTY%\stb %THIRDPARTY%\arnold_windows\lib\ai.lib /link /out:lambda.exe
-popd
-
--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-clang main.cpp \
-    -std=c++14 \
-    -lm -lstdc++ \
-    -I../thirdparty/arnold_linux/include \
-    -I../thirdparty/stb \
-    -L../thirdparty/arnold_linux/bin -lai \
-    -o ../build/lambda
